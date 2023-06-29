5490fe16166d6b3be8fdfb49d65c11410723061b
dlobosm <diegolobos@MacBook-Pro-de-Diego.local>
2023-06-28 21:26:32 -0400

    avance

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  resources :colaborators do
+    member do
+      get "preview"
+    end
+    collection do
+      get 'search'
+end 
+end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+    root "colaborators#index"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
